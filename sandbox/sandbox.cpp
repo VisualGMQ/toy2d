dafb0a31f0a6c2d2e7482d1f7dc279b651cb46ba
VisualGMQ <2142587070@qq.com>
2022-11-29 23:08:32 +0800

    basic framework

--- sandbox/sandbox.cpp
+++ sandbox/sandbox.cpp
@@ -1,7 +1,5 @@
-#include "toy2d/toy2d.hpp"
 #include "SDL.h"
 #include "SDL_vulkan.h"
-#include <SDL_video.h>
 
 int main(int argc, char** argv) {
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -14,59 +12,16 @@ int main(int argc, char** argv) {
         SDL_Log("create window failed");
         exit(2);
     }
-
-    unsigned int count;
-    SDL_Vulkan_GetInstanceExtensions(window, &count, nullptr);
-    std::vector<const char*> extensions(count);
-    SDL_Vulkan_GetInstanceExtensions(window, &count, extensions.data());
-
-    toy2d::Init(extensions,
-        [&](VkInstance instance){
-            VkSurfaceKHR surface;
-            SDL_Vulkan_CreateSurface(window, instance, &surface);
-            return surface;
-        }, 1024, 720);
-    auto renderer = toy2d::GetRenderer();
-
     bool shouldClose = false;
     SDL_Event event;
 
-    float x = 100, y = 100;
-
-    renderer->SetDrawColor(toy2d::Color{0, 1, 0});
     while (!shouldClose) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 shouldClose = true;
             }
-            if (event.type == SDL_KEYDOWN) {
-                if (event.key.keysym.sym == SDLK_a) {
-                    x -= 10;
-                }
-                if (event.key.keysym.sym == SDLK_d) {
-                    x += 10;
-                }
-                if (event.key.keysym.sym == SDLK_w) {
-                    y -= 10;
-                }
-                if (event.key.keysym.sym == SDLK_s) {
-                    y += 10;
-                }
-                if (event.key.keysym.sym == SDLK_0) {
-                    renderer->SetDrawColor(toy2d::Color{1, 0, 0});
-                }
-                if (event.key.keysym.sym == SDLK_1) {
-                    renderer->SetDrawColor(toy2d::Color{0, 1, 0});
-                }
-                if (event.key.keysym.sym == SDLK_2) {
-                    renderer->SetDrawColor(toy2d::Color{0, 0, 1});
-                }
-            }
         }
-        renderer->DrawRect(toy2d::Rect{toy2d::Vec{x, y},
-                                       toy2d::Size{200, 300}});
     }
-    toy2d::Quit();
 
     SDL_DestroyWindow(window);
     SDL_Quit();
--- src/toy2d.cpp
+++ src/toy2d.cpp
@@ -1,31 +1,10 @@
 #include "toy2d/toy2d.hpp"
+#include <iostream>
 
 namespace toy2d {
 
-std::unique_ptr<Renderer> renderer_;
-
-void Init(std::vector<const char*>& extensions, Context::GetSurfaceCallback cb, int windowWidth, int windowHeight) {
-    Context::Init(extensions, cb);
-    auto& ctx = Context::Instance();
-    ctx.initSwapchain(windowWidth, windowHeight);
-    ctx.initShaderModules();
-    ctx.initRenderProcess();
-    ctx.initGraphicsPipeline();
-    ctx.swapchain->InitFramebuffers();
-    ctx.initCommandPool();
-
-    renderer_ = std::make_unique<Renderer>();
-    renderer_->SetProject(windowWidth, 0, 0, windowHeight, -1, 1);
-}
-
-void Quit() {
-    Context::Instance().device.waitIdle();
-    renderer_.reset();
-    Context::Quit();
-}
-
-Renderer* GetRenderer() {
-    return renderer_.get();
+void Init() {
+    std::cout << "Hello Toy2D" << std::endl;
 }
 
 }
